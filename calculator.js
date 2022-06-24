91bdc6aff94c1cecdebf466b6c8d20d2994b84ed
cianajoy <105308719+cianajoy@users.noreply.github.com>
2022-06-24 22:09:06 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,32 @@
+var first = window.prompt("Enter First Number:");
+var second = window.prompt("Enter Second Number:");
+var operator = window.prompt("Enter Operator:");
+calculate();
+
+function calculate() {
+    if (operator == "+") {
+        var result = parseInt(first) + parseInt(second);
+        alert(first +" + " + second + " = " + result)
+    }
+
+    else if (operator == "-") {
+        var result = parseInt(first) - parseInt(second);
+        alert(first +" - " + second + " = " + result)
+    }
+
+    else if (operator == "*") {
+        var result = parseInt(first) * parseInt(second);
+        alert(first + " * " + second + " = " + result)
+    }
+
+    else if (operator == "/") {
+        var result = parseInt(first) / parseInt(second);
+        alert(first +" / " + second + " = " + result)
+    }
+
+    else {
+        alert(operator + " IS NOT A VALID OPERATOR, TRY AGAIN")
+    }
+
+}
+
